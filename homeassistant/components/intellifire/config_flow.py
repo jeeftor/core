63fd842588e5cb23ab9815fe05a5fffcd1b9f567
Jeef <jeeftor@user.noreply.github.com>
2022-02-18 04:22:43 -0700

    DHCP Discovery for Intellifire Devices

--- homeassistant/components/intellifire/config_flow.py
+++ homeassistant/components/intellifire/config_flow.py
@@ -8,13 +8,12 @@ from intellifire4py import IntellifireAsync
 import voluptuous as vol
 
 from homeassistant import config_entries
+from homeassistant.components.dhcp import DhcpServiceInfo
 from homeassistant.const import CONF_HOST
 from homeassistant.core import HomeAssistant
 from homeassistant.data_entry_flow import FlowResult
 
-from .const import DOMAIN
-
-STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_HOST): str})
+from .const import DOMAIN, LOGGER
 
 
 async def validate_input(hass: HomeAssistant, host: str) -> str:
@@ -29,10 +28,45 @@ async def validate_input(hass: HomeAssistant, host: str) -> str:
     return api.data.serial
 
 
-class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
+async def validate_host_input(host: str) -> str:
+    """Validate the user input allows us to connect.
+
+    Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
+    """
+    api = IntellifireAsync(host)
+    await api.poll()
+    ret = api.data.serial
+    LOGGER.info("Found a fireplace: %s", ret)
+    # Return the serial number which will be used to calculate a unique ID for the device/sensors
+    return ret
+
+
+STEP_USER_DATA_SCHEMA = vol.Schema({vol.Required(CONF_HOST): str})
+
+
+class IntellifireConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     """Handle a config flow for IntelliFire."""
 
     VERSION = 1
+
+    async def async_step_dhcp(self, discovery_info: DhcpServiceInfo) -> FlowResult:
+        """Handle DHCP Discovery."""
+        potential_host = discovery_info.ip
+
+        try:
+            serial = await validate_host_input(potential_host)
+        except (ConnectionError, ClientConnectionError):
+            return self.async_abort(reason="not_intellifire_device")
+
+        await self.async_set_unique_id(serial)
+        # check if found before
+        self._abort_if_unique_id_configured(
+            updates={
+                CONF_HOST: potential_host,
+            }
+        )
+
+        return await self.async_step_user()
 
     async def async_step_user(
         self, user_input: dict[str, Any] | None = None
