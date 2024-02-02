ba03a344d92ffe1627fb83f29e644d52681d3e2c
Jeef <jeeftor@users.noreply.github.com>
2024-02-02 08:53:32 -0700

    Update tests/components/simplefin/test_init.py
    Co-authored-by: Josef Zweck <24647999+zweckj@users.noreply.github.com>

--- tests/components/simplefin/test_init.py
+++ tests/components/simplefin/test_init.py
@@ -20,7 +20,6 @@ async def test_init(
     await hass.config_entries.async_setup(mock_config_entry.entry_id)
     assert mock_config_entry.state == ConfigEntryState.LOADED
 
-    # How would I test all the sensors?
 
     await hass.config_entries.async_unload(mock_config_entry.entry_id)
     await hass.async_block_till_done()
